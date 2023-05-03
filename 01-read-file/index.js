890b65dfecb6ed59a9ad2c9856fa4c6e8378514e
Artem Pukhov <art.puhov@gmail.com>
2023-05-04 02:44:53 +0300

    feat: done 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+
+const fs = require('fs');
+
+const readStream = fs.createReadStream(filePath, 'utf-8');
+
+readStream.on('data', (chunk) => {
+  console.log(chunk);
+});
+
+readStream.on('error', (error) => {
+  console.log(error);
+});
+
+readStream.on('end', () => {
+    console.log('File reading is complete.');
+  });
