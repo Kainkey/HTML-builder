d186a70f6f2c595f310a22bbc61116ea8ad1888e
Artem Pukhov <art.puhov@gmail.com>
2023-05-08 23:35:58 +0300

    feat: done 04-files-in-folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourceDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
+
+function copyDirectoryRecursive(source, target) {
+  // Создаем директорию если она не существует
+  if (!fs.existsSync(target)) {
+    fs.mkdirSync(target);
+  }
+
+  // Читаем содержимое исходной директории
+  fs.readdirSync(source).forEach(name => {
+    const sourcePath = path.join(source, name);
+    const targetPath = path.join(target, name);
+
+    // Если элемент является директорией, рекурсивно копируем её содержимое
+    if (fs.lstatSync(sourcePath).isDirectory()) {
+      copyDirectoryRecursive(sourcePath, targetPath);
+    } else { // Если элемент является файлом, копируем его
+      fs.copyFileSync(sourcePath, targetPath);
+    }
+  });
+}
+
+copyDirectoryRecursive(sourceDir, targetDir);
+console.log('Копирование завершено.');
 
